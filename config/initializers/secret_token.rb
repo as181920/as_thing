92027ae972df3d0bbb9e2454442f31237f6e02b7
Andersen Fan <as181920@hotmail.com>
2011-05-29 11:28:23 +0800

    rails new as_thing;add empty dir for tmp and log

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsThing::Application.config.secret_token = 'f4e8213b3126548166e908f52641668e753590229105201ea62766bcebc1e81d36d4dbb986fbf31822e5612bb0ae741f08d64f598b87fca6967b979d1daf8c97'
