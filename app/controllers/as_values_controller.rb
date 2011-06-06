7e3b9afccd3013959990e094f11d7ae6e2a8efab
Andersen Fan <as181920@hotmail.com>
2011-06-06 12:35:35 +0800

    done with empty label,value,note

--- app/controllers/as_values_controller.rb
+++ app/controllers/as_values_controller.rb
@@ -6,11 +6,10 @@ class AsValuesController < ApplicationController
     @labels = @as_note.get_sorted_labels(@as_note)
     #@numeros = @as_note.as_labels.first.as_values.all(:select=>"numero",:order=>"numero DESC").collect{|n| n.numero}
     #todo ..when label is nil
-    @l1_values = @as_note.as_labels.first.as_values.all(:select=>"id,numero,value", :order=>"numero DESC")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      #format.xml  { render :xml => @as_values }
+    unless @as_note.as_labels.first
+      redirect_to(@as_note, :notice =>'Should add labels first.')
+    else
+      @l1_values = @as_note.as_labels.first.as_values.all(:select=>"id,numero,value", :order=>"numero DESC")
     end
   end
 
